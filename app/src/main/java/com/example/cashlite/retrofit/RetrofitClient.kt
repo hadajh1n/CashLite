08dbf82488c912fd80634fb08990297cf2e08d37
hadajh1n <anton_sheludchenkov@mail.ru>
2025-11-04 21:07:06 +0300

    transition to the ngrok api

--- app/src/main/java/com/example/cashlite/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/cashlite/retrofit/RetrofitClient.kt
@@ -1,14 +1,25 @@
 package com.example.cashlite.retrofit
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.70:5000/"
+    private const val BASE_URL = "https://kayce-superoratorical-omari.ngrok-free.dev/"
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("ngrok-skip-browser-warning", "true")
+                .build()
+            chain.proceed(request)
+        }
+        .build()
 
     val transactionApi: TransactionApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(TransactionApi::class.java)
