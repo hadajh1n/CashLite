0b24d707959f86c25c52c214531d89e04646460e
hadajh1n <anton_sheludchenkov@mail.ru>
2026-03-11 22:08:29 +0300

    changed navigation style for activity_add_new_operation

--- app/src/main/java/com/example/cashlite/core/utils/NavigationAddNewOperation.kt
+++ app/src/main/java/com/example/cashlite/core/utils/NavigationAddNewOperation.kt
@@ -1,15 +1,42 @@
 package com.example.cashlite.core.utils
 
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.example.cashlite.R
 import com.example.cashlite.databinding.ActivityAddNewOperationBinding
 
-fun AppCompatActivity.setupAddOperationNavigation(binding: ActivityAddNewOperationBinding) {
+fun AppCompatActivity.setupAddOperationNavigation(
+    binding: ActivityAddNewOperationBinding
+) = with(binding) {
+
     val navHostFragment = supportFragmentManager
         .findFragmentById(R.id.nav_host_add_new_operation_fragment) as NavHostFragment
 
-    binding.topNavigationOperation
-        .setupWithNavController(navHostFragment.navController)
+    val navController = navHostFragment.navController
+
+    fun selectButton(selected: View) = with(binding) {
+        btnExpense.isSelected = false
+        btnIncome.isSelected = false
+        btnTransfer.isSelected = false
+
+        selected.isSelected = true
+    }
+
+    btnExpense.setOnClickListener {
+        selectButton(btnExpense)
+        navController.navigate(R.id.addExpenceOperationFragment)
+    }
+
+    btnIncome.setOnClickListener {
+        selectButton(btnIncome)
+        navController.navigate(R.id.addIncomeOperationFragment)
+    }
+
+    btnTransfer.setOnClickListener {
+        selectButton(btnTransfer)
+        navController.navigate(R.id.addTransferOperationFragment)
+    }
+
+    btnExpense.isSelected = true
 }
